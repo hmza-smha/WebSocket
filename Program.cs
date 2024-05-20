db9a852c13e09392b9b11454cf440c3596d99a1d
HamZa Samha <smha.hmza@gmail.com>
2024-05-20 17:04:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+class WebSocketServer
+{
+    public static async Task Main(string[] args)
+    {
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:5000/ws/");
+        listener.Start();
+        Console.WriteLine("WebSocket server started at ws://localhost:5000/ws/");
+
+        while (true)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+            if (context.Request.IsWebSocketRequest)
+            {
+                HttpListenerWebSocketContext wsContext = await context.AcceptWebSocketAsync(null);
+                WebSocket webSocket = wsContext.WebSocket;
+                await HandleWebSocketConnection(webSocket);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+            }
+        }
+    }
+
+    private static async Task HandleWebSocketConnection(WebSocket webSocket)
+    {
+        byte[] buffer = new byte[1024];
+        WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+        while (result.MessageType != WebSocketMessageType.Close)
+        {
+            string receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+            Console.WriteLine("Received: " + receivedMessage);
+
+            string responseMessage = "Echo: " + receivedMessage;
+            byte[] responseBuffer = Encoding.UTF8.GetBytes(responseMessage);
+            await webSocket.SendAsync(new ArraySegment<byte>(responseBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+        }
+
+        await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", CancellationToken.None);
+    }
+}
